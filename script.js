597046eca691460fd387c7689c3454b57c4bcfab
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2025-10-24 15:15:19 +1100

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,18 @@ const movie3 = new movie(4, "Great Grandson of the Mask", 2027, 0.6);
 const movie4 = new movie(5, "Greater Grandson of the Mask", 2038, 0.3);
 const movie9 = new movie(6, "Greaterer Grandson of the Mask", 2049, 0.1);
 const movie1 = new movie(7, "Greatererer Grandson of the Mask", 2060, -0.1);
-const movie8 = new movie(7, "Greaterererer Grandson of the Mask", 2071, -0.6);
-const movie6 = new movie(7, "Greatererererer Grandson of the Mask", 2082, -1.2);
-const movie2 = new movie(7, "Greaterererererer Grandson of the Mask", 2093, -5.0);
+const movie8 = new movie(8, "Greaterererer Grandson of the Mask", 2071, -0.6);
+const movie6 = new movie(9, "Greatererererer Grandson of the Mask", 2082, -1.2);
+const movie2 = new movie(10, "Greaterererererer Grandson of the Mask", 2093, -5.0);
 
 // Creates an array and stores the movies inside it
-const maskMovies = [
+let maskMovies = [
   movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10
 ];
 // Logs out the array
-console.log("The array of movies is: " + maskMovies);
+console.log(maskMovies);
+
+// Sorts the array by movieID in ascending order
+maskMovies = maskMovies.sort((a, b) => a.movieID - b.movieID);
+// Logs out the sorted array
+console.log(maskMovies);
