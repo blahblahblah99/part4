a7c79d13fe16e2e5455046b1e4e156614cc49b87
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2025-10-24 14:29:06 +1100

    movie class made

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // File: script.js
-// Desc: This file shows how to operate an array of objects
+// Desc: This file shows how to use and manipulate complex data types
 // Author: Keegan Hughes
 // Date: 24/10/2025
 // Modified: 24/10/2025
 
+// Defines a movie class with the properties movieID, title, year, and rating
+class movie {
+  constructor(movieID, title, year, rating) {
+    this.movieID = movieID;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  }
+}
